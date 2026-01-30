14684bc919ee68aa130eddd93be55571241b4e07
gevrekliilkokulu738881-beep <gevrekliilkokulu738881@gmail.com>
2026-01-30 07:10:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,67 +14,63 @@ let tournamentQueue = [];
 io.on('connection', (socket) => {
     socket.emit('roomList', getPublicRooms());
 
-    socket.on('createRoom', (data) => {
-        const { roomName, password, playerName } = data;
-        if (rooms[roomName]) return socket.emit('errorMsg', 'Bu masa adı dolu!');
-        rooms[roomName] = {
-            password: password || "",
-            hasPass: !!password,
-            players: {}, 
-            owner: socket.id,
-            ready: { mor: false, turuncu: false }
-        };
-        socket.emit('roomCreated', roomName);
-        io.emit('roomList', getPublicRooms());
-    });
-
-    socket.on('joinGame', (data) => {
-        const room = rooms[data.roomName];
-        if (!room) return socket.emit('errorMsg', 'Masa bulunamadı!');
-        if (Object.keys(room.players).length >= 2) return socket.emit('errorMsg', 'Masa dolu!');
-        if (room.hasPass && room.password !== data.password) return socket.emit('errorMsg', 'Şifre yanlış!');
-
-        const role = Object.keys(room.players).length === 0 ? 'mor' : 'turuncu';
-        room.players[socket.id] = { role, name: data.playerName };
-        socket.role = role;
-        socket.room = data.roomName;
-        socket.playerName = data.playerName;
-        socket.join(data.roomName);
-        socket.emit('playerRole', { role, roomName: data.roomName, playerName: data.playerName });
-    });
-
+    // --- TURNUVA KATILIM ---
     socket.on('joinTournament', (playerName) => {
+        // Eğer oyuncu zaten listede yoksa ekle
         if (!tournamentQueue.find(p => p.id === socket.id)) {
             tournamentQueue.push({ id: socket.id, name: playerName });
+            console.log("Turnuva Kuyruğu:", tournamentQueue.length);
             io.emit('tourUpdate', tournamentQueue.length);
             
             if (tournamentQueue.length === 4) {
-                const tourID = "TOUR_" + Math.floor(Math.random() * 1000);
+                const tourID = "TOUR_" + Math.floor(Math.random() * 9999);
                 const p = [...tournamentQueue];
-                tournamentQueue = [];
-                // Eşleşmeleri yap ve gönder
-                io.to(p[0].id).emit('startTourMatch', { room: tourID+"_M1", opp: p[1].name });
-                io.to(p[1].id).emit('startTourMatch', { room: tourID+"_M1", opp: p[0].name });
-                io.to(p[2].id).emit('startTourMatch', { room: tourID+"_M2", opp: p[3].name });
-                io.to(p[3].id).emit('startTourMatch', { room: tourID+"_M2", opp: p[2].name });
+                tournamentQueue = []; // Kuyruğu sıfırla
+
+                // Odaları sunucu tarafında oluştur
+                rooms[tourID + "_M1"] = { password: "", hasPass: false, players: {}, owner: "SYSTEM", ready: { mor: false, turuncu: false } };
+                rooms[tourID + "_M2"] = { password: "", hasPass: false, players: {}, owner: "SYSTEM", ready: { mor: false, turuncu: false } };
+
+                // Oyuncuları eşleştir ve zorla yönlendir
+                io.to(p[0].id).emit('startTourMatch', { room: tourID + "_M1" });
+                io.to(p[1].id).emit('startTourMatch', { room: tourID + "_M1" });
+                io.to(p[2].id).emit('startTourMatch', { room: tourID + "_M2" });
+                io.to(p[3].id).emit('startTourMatch', { room: tourID + "_M2" });
+                
                 io.emit('tourUpdate', 0);
             }
         }
     });
 
+    // --- STANDART GİRİŞ ---
+    socket.on('joinGame', (data) => {
+        if (!rooms[data.roomName]) return socket.emit('errorMsg', 'Masa bulunamadı!');
+        const room = rooms[data.roomName];
+        
+        if (Object.keys(room.players).length >= 2) return socket.emit('errorMsg', 'Masa dolu!');
+        
+        const role = Object.keys(room.players).length === 0 ? 'mor' : 'turuncu';
+        room.players[socket.id] = { role, name: data.playerName };
+        socket.role = role;
+        socket.room = data.roomName;
+        
+        socket.join(data.roomName);
+        socket.emit('playerRole', { role, roomName: data.roomName });
+    });
+
+    // --- SOHBET ---
     socket.on('chatMsg', (data) => {
-        const badWords = ["küfür1", "küfür2"]; 
-        let cleanMsg = data.msg;
-        badWords.forEach(w => cleanMsg = cleanMsg.replace(new RegExp(w, 'gi'), '***'));
-        io.to(data.roomID).emit('newChat', { user: data.user, msg: cleanMsg });
+        io.to(data.roomID).emit('newChat', { user: data.user, msg: data.msg, type: data.type });
     });
 
-    socket.on('kickPlayer', (roomID) => {
-        if (rooms[roomID] && rooms[roomID].owner === socket.id) {
-            socket.to(roomID).emit('banned');
-        }
+    // ... (createRoom, playerReady, move ve disconnect kodları öncekiyle aynı kalsın)
+    socket.on('createRoom', (data) => {
+        const { roomName, password } = data;
+        if (rooms[roomName]) return socket.emit('errorMsg', 'Bu isimde masa var!');
+        rooms[roomName] = { password: password || "", hasPass: !!password, players: {}, owner: socket.id, ready: { mor: false, turuncu: false }};
+        socket.emit('roomCreated', roomName);
+        io.emit('roomList', getPublicRooms());
     });
-
     socket.on('playerReady', (d) => {
         const room = rooms[d.roomID];
         if(room) {
@@ -82,13 +78,7 @@ io.on('connection', (socket) => {
             io.to(d.roomID).emit('updateReadyStatus', { morReady: room.ready.mor, turuncuReady: room.ready.turuncu });
         }
     });
-
     socket.on('move', (d) => socket.to(d.roomID).emit('opponentMove', d));
-
-    socket.on('disconnect', () => {
-        tournamentQueue = tournamentQueue.filter(p => p.id !== socket.id);
-        io.emit('tourUpdate', tournamentQueue.length);
-    });
 });
 
 function getPublicRooms() {
