ae4343e6c165de07961ec861ed404d94dcb44c40
gevrekliilkokulu738881-beep <gevrekliilkokulu738881@gmail.com>
2026-01-30 05:54:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,9 @@ const path = require('path');
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, {
-    cors: { origin: "*" }
-});
+const io = new Server(httpServer, { cors: { origin: "*" } });
 
-// HTML ve diğer dosyaları sunmak için
+// Dosyaları sunucunun içine gömüyoruz
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
@@ -19,50 +17,47 @@ app.get('/', (req, res) => {
 let masalar = {};
 
 io.on('connection', (socket) => {
-    socket.emit('liste_guncelle', masalar);
+    // Yeni girene masaları göster
+    socket.emit('liste', masalar);
 
-    socket.on('masa_kur', (data) => {
+    socket.on('kur', (data) => {
         const odaId = "oda_" + socket.id;
         socket.join(odaId);
         masalar[odaId] = { 
             id: odaId, 
-            morIsim: data.isim || "Oyuncu 1", 
-            turuncuIsim: "", 
+            mor: data.isim || "Oyuncu 1", 
+            turuncu: "", 
             durum: 'bekliyor' 
         };
-        io.emit('liste_guncelle', masalar);
-        socket.emit('bekleme_modu');
+        io.emit('liste', masalar);
+        socket.emit('kuruldu', odaId);
     });
 
-    socket.on('masaya_otur', (data) => {
+    socket.on('katil', (data) => {
         const id = data.id;
         if (masalar[id] && masalar[id].durum === 'bekliyor') {
             socket.join(id);
-            masalar[id].turuncuIsim = data.isim || "Oyuncu 2";
+            masalar[id].turuncu = data.isim || "Oyuncu 2";
             masalar[id].durum = 'dolu';
-            io.emit('liste_guncelle', masalar);
-            io.to(id).emit('oyun_basla', { 
-                oda: id, 
-                morIsim: masalar[id].morIsim, 
-                turuncuIsim: masalar[id].turuncuIsim 
-            });
+            io.emit('liste', masalar);
+            io.to(id).emit('basla', masalar[id]);
         }
     });
 
-    socket.on('hamle_yap', (data) => {
-        socket.to(data.oda).emit('hamle_geldi', data);
+    socket.on('hamle', (data) => {
+        socket.to(data.oda).emit('rakip_hamle', data);
     });
 
     socket.on('disconnect', () => {
         for (let id in masalar) {
             if (id.includes(socket.id)) {
-                socket.to(id).emit('rakip_ayrildi');
+                socket.to(id).emit('koptu');
                 delete masalar[id];
             }
         }
-        io.emit('liste_guncelle', masalar);
+        io.emit('liste', masalar);
     });
 });
 
 const PORT = process.env.PORT || 3000;
-httpServer.listen(PORT, "0.0.0.0", () => console.log("Server Live on " + PORT));
+httpServer.listen(PORT, () => console.log("Sistem Hazir!"));
