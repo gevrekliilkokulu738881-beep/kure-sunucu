09c5deaeb96cba17aac1b5ffc2339f40e1abfa82
gevrekliilkokulu738881-beep <gevrekliilkokulu738881@gmail.com>
2026-01-30 04:49:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,19 +7,16 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, { cors: { origin: "*" } });
 
 let masalar = {}; 
+let socketOdaMap = {}; 
 
 io.on('connection', (socket) => {
     socket.emit('liste_guncelle', masalar);
 
     socket.on('masa_kur', (data) => {
-        const odaId = "oda_" + socket.id; // Benzersiz oda ID
+        const odaId = "oda_" + socket.id;
         socket.join(odaId);
-        masalar[odaId] = { 
-            id: odaId, 
-            kurucu: socket.id,
-            isim: data.isim || "İsimsiz", 
-            durum: 'bekliyor'
-        };
+        masalar[odaId] = { id: odaId, kurucu: socket.id, isim: data.isim || "İsimsiz", durum: 'bekliyor' };
+        socketOdaMap[socket.id] = odaId;
         io.emit('liste_guncelle', masalar);
         socket.emit('bekleme_modu', { odaId });
     });
@@ -29,32 +26,28 @@ io.on('connection', (socket) => {
         if (masalar[odaId] && masalar[odaId].durum === 'bekliyor') {
             socket.join(odaId);
             masalar[odaId].durum = 'dolu';
+            socketOdaMap[socket.id] = odaId;
             io.emit('liste_guncelle', masalar);
-            // Odadaki herkese (kurucu ve katılan) başlat gönder
             io.to(odaId).emit('oyun_basla', { oda: odaId });
         }
     });
 
-    socket.on('hamle_yap', (data) => {
-        // Hamleyi odadaki diğer kişiye gönder
-        socket.to(data.oda).emit('hamle_geldi', data);
-    });
+    socket.on('hamle_yap', (data) => socket.to(data.oda).emit('hamle_geldi', data));
+    socket.on('chat_gonder', (data) => io.to(data.oda).emit('chat_geldi', data));
 
-    socket.on('chat_gonder', (data) => {
-        io.to(data.oda).emit('chat_geldi', data);
-    });
+    const ayrilmaIslemi = () => {
+        const odaId = socketOdaMap[socket.id];
+        if (odaId) {
+            socket.to(odaId).emit('rakip_ayrildi');
+            delete masalar[odaId];
+            delete socketOdaMap[socket.id];
+            io.emit('liste_guncelle', masalar);
+        }
+    };
 
-    socket.on('disconnect', () => {
-        // Hangi odadaysa oraya ayrıldı gönder ve masayı sil
-        Object.keys(masalar).forEach(odaId => {
-            if (masalar[odaId].kurucu === socket.id || socket.rooms.has(odaId)) {
-                io.to(odaId).emit('rakip_ayrildi');
-                delete masalar[odaId];
-            }
-        });
-        io.emit('liste_guncelle', masalar);
-    });
+    socket.on('disconnect', ayrilmaIslemi);
+    socket.on('masayi_kapat', ayrilmaIslemi);
 });
 
 const PORT = process.env.PORT || 3000;
-httpServer.listen(PORT, () => console.log(`Sunucu 2026 stabil.`));
+httpServer.listen(PORT, () => console.log(`Sunucu Eksiksiz Modda Yayında.`));
