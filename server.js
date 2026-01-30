6b5b63b6533f41cd4a49516dcf6e99b0e2bd70e6
gevrekliilkokulu738881-beep <gevrekliilkokulu738881@gmail.com>
2026-01-30 05:23:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,38 +5,46 @@ const path = require('path');
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, { cors: { origin: "*" } });
+const io = new Server(httpServer, {
+    cors: { origin: "*" } // Dışarıdan bağlantı engellerini kaldırır
+});
 
-app.use(express.static(__dirname));
+// index.html dosyasını aynı klasörde tut
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 let masalar = {}; 
 let socketOdaMap = {}; 
 let revansIstekleri = {};
 
 io.on('connection', (socket) => {
+    console.log('Bağlantı başarılı:', socket.id);
     socket.emit('liste_guncelle', masalar);
 
     socket.on('masa_kur', (data) => {
         const odaId = "oda_" + socket.id;
         socket.join(odaId);
-        masalar[odaId] = { id: odaId, kurucu: socket.id, isim: data.isim || "Oyuncu", durum: 'bekliyor' };
+        masalar[odaId] = { id: odaId, kurucu: socket.id, isim: data.isim || "Anonim", durum: 'bekliyor' };
         socketOdaMap[socket.id] = odaId;
         io.emit('liste_guncelle', masalar);
         socket.emit('bekleme_modu');
+        console.log('Masa kuruldu:', odaId);
     });
 
-    socket.on('masaya_otur', (data) => {
-        const odaId = data.id;
-        if (masalar[odaId] && masalar[odaId].durum === 'bekliyor') {
-            socket.join(odaId);
-            masalar[odaId].durum = 'dolu';
-            socketOdaMap[socket.id] = odaId;
+    socket.on('masaya_otur', (id) => {
+        if (masalar[id] && masalar[id].durum === 'bekliyor') {
+            socket.join(id);
+            masalar[id].durum = 'dolu';
+            socketOdaMap[socket.id] = id;
             io.emit('liste_guncelle', masalar);
-            io.to(odaId).emit('oyun_basla', { oda: odaId });
+            io.to(id).emit('oyun_basla', { oda: id });
         }
     });
 
-    socket.on('hamle_yap', (data) => socket.to(data.oda).emit('hamle_geldi', data));
+    socket.on('hamle_yap', (data) => {
+        socket.to(data.oda).emit('hamle_geldi', data);
+    });
 
     socket.on('revans_iste', (data) => {
         if(!revansIstekleri[data.oda]) revansIstekleri[data.oda] = {};
@@ -53,9 +61,11 @@ io.on('connection', (socket) => {
         if (odaId) {
             socket.to(odaId).emit('rakip_ayrildi');
             delete masalar[odaId]; delete socketOdaMap[socket.id];
-            io.emit('liste_guncelle', masalar);
         }
+        io.emit('liste_guncelle', masalar);
     });
 });
 
-httpServer.listen(3000, () => console.log("Sunucu 3000 portunda hazır."));
+httpServer.listen(3000, () => {
+    console.log("SERVER AKTİF: http://localhost:3000 adresine git!");
+});
